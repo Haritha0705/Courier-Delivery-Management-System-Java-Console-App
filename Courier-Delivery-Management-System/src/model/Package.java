0564c150231d0c9cb98a31a7ef6a285d340e85b1
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-07-13 08:35:42 +0530

    done in package class

--- Courier-Delivery-Management-System/src/model/Package.java
+++ Courier-Delivery-Management-System/src/model/Package.java
@@ -1,4 +1,60 @@
 package model;
 
 public class Package {
+    private final String packageId;
+    private String senderName;
+    private String receiverName;
+    private String address;
+    private double weight;
+    private String type;
+    private String status;
+    private DeliveryAgent assignedAgent;
+
+    //constructor
+    public Package(String packageId, String senderName, String receiverName, String address, double weight, String type, String status, DeliveryAgent assignedAgent){
+        this.packageId = packageId;
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.address = address;
+        this.weight = weight;
+        this.type = type;
+        this.status = status;
+        this.assignedAgent = assignedAgent;
+    }
+
+    //crete getter and setters
+    public String getPackageId(){
+        return packageId;
+    }
+    public String setSenderName(String senderName){
+        this.senderName=senderName;
+        return senderName;
+    }
+    public String setReceiverName(String receiverName){
+        this.receiverName=receiverName;
+        return receiverName;
+    }
+
+    public String setAddress(String address){
+        this.address=address;
+        return address;
+    }
+    public double setWeight(double weight){
+        this.weight=weight;
+        return weight;
+    }
+
+    public String setType(String type){
+        this.type=type;
+        return type;
+    }
+    public String setStatus(String status){
+        this.status=status;
+        return status;
+    }
+    public DeliveryAgent setAssignedAgent(DeliveryAgent assignedAgent){
+        this.assignedAgent=assignedAgent;
+        return assignedAgent;
+    }
+
 }
