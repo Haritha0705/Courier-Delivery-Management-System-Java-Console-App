60ae2bd364955962b34764bf2f2c3e671e44e1c4
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-07-17 11:11:33 +0530

    SearchPackageID method in AgentPanel class

--- Courier-Delivery-Management-System/src/ui/CustomerPortal.java
+++ Courier-Delivery-Management-System/src/ui/CustomerPortal.java
@@ -1,4 +1,31 @@
 package ui;
 
+import model.DeliveryAgent;
+import model.Package;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CustomerPortal {
+    private static List<Package> packages = new ArrayList<>();
+    private static List<DeliveryAgent> deliveryAgents = new ArrayList<>();
+
+    public static void SearchPackageID(Scanner scanner){
+        System.out.println("Enter Package Id");
+        String id = scanner.next();
+
+        for (Package p : packages){
+            System.out.println("Package ID: " + p.getPackageId());
+            System.out.println("Sender: " + p.getSenderName());
+            System.out.println("Receiver: " + p.getReceiverName());
+            System.out.println("Address: " + p.getAddress());
+            System.out.println("Weight: " + p.getWeight());
+            System.out.println("Type: " + p.getType());
+            System.out.println("Status: " + p.getStatus());
+            System.out.println("-----------------------------------");
+            return;
+        }
+        System.out.println("Package not found.");
+    }
 }
